e7486800374d9db2378e538f458ca304249e5070
Nicholas H.R. Sims <nickhrsims@gmail.com>
2024-02-17 14:39:10 +0000

    docs: comments

--- src/core/app.h
+++ src/core/app.h
@@ -29,15 +29,38 @@ class App {
     App(App&& app)             = delete;
     App& operator=(const App&) = delete;
 
+    /**
+     * Start the `App` instance, executing primary frame/event processing loop.
+     * \sa App::stop
+     */
     void start(const Config& config);
+
+    /**
+     * Stop a previously started (and still running) `App`.
+     * Does nothing if app has not been started (and/or is not running).
+     *
+     * \sa App::start
+     */
     void stop();
 
-    virtual void processFrame(float delta)      = 0;
+    /**
+     * Virtual frame processor.
+     *
+     * Override to describe how to process a single frame.
+     */
+    virtual void processFrame(float delta) = 0;
+
+    /**
+     * Virtual event processor.
+     *
+     * Override to describe how to process a single event.
+     */
     virtual void processEvent(SDL_Event& event) = 0;
 
   protected:
     App();
 
   private:
+    /** Internal flag used for control-flow. */
     bool isRunning;
 };
