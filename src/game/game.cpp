60c0293218baded7c915122fb6da5b88f1b659d5
Nicholas H.R. Sims <nickhrsims@gmail.com>
2024-02-18 23:28:42 +0000

    feat: prototype game fsm

--- src/game/game.cpp
+++ src/game/game.cpp
@@ -1,3 +1,5 @@
+#include <cassert>
+
 #include <spdlog/spdlog.h>
 
 #include "game.h"
@@ -6,21 +8,155 @@
 // Constructor / Destructor
 // -----------------------------------------------------------------------------
 
-Game::Game() : App{} {}
+Game::Game() : App{}, currentState{&startState} {
+    // ---------------------------------
+    // State Transitions
+    // ---------------------------------
+
+    // --- Start
+    startState.onConfirm = &fieldSetupState;
+    startState.onQuit    = &shutdownState;
+
+    // --- Reset
+    resetState.onNext = &fieldSetupState;
+    resetState.onQuit = &shutdownState;
+
+    // --- Field Setup
+    fieldSetupState.onNext = &countdownState;
+    fieldSetupState.onQuit = &shutdownState;
+
+    // --- Countdown
+    countdownState.onNext = &playingState;
+    countdownState.onQuit = &shutdownState;
+
+    // --- Playing
+    playingState.onPause    = &pauseState;
+    playingState.onNext     = &fieldSetupState;
+    playingState.onGameOver = &gameOverState;
+    playingState.onQuit     = &shutdownState;
+
+    // --- Pause
+    pauseState.onPause = &playingState;
+    pauseState.onQuit  = &shutdownState;
+
+    // --- Game Over
+    gameOverState.onConfirm = &resetState;
+    gameOverState.onCancel  = &shutdownState;
+    gameOverState.onQuit    = &shutdownState;
+
+    // ---------------------------------
+    // State Execution Controls
+    // ---------------------------------
+
+    // --- Start
+    startState.enter        = []() {};
+    startState.exit         = []() {};
+    startState.processFrame = [](const float delta) { (void)delta; };
+    startState.processEvent = [](const SDL_Event& event) { (void)event; };
+
+    // --- Reset
+    resetState.enter        = []() {};
+    resetState.exit         = []() {};
+    resetState.processFrame = [](const float delta) { (void)delta; };
+    resetState.processEvent = [](const SDL_Event& event) { (void)event; };
+
+    // --- Countdown
+    countdownState.enter        = []() {};
+    countdownState.exit         = []() {};
+    countdownState.processFrame = [](const float delta) { (void)delta; };
+    countdownState.processEvent = [](const SDL_Event& event) { (void)event; };
+
+    // --- Field Setup
+    fieldSetupState.enter        = []() {};
+    fieldSetupState.exit         = []() {};
+    fieldSetupState.processFrame = [](const float delta) { (void)delta; };
+    fieldSetupState.processEvent = [](const SDL_Event& event) { (void)event; };
+
+    // --- Playing
+    playingState.enter        = []() {};
+    playingState.exit         = []() {};
+    playingState.processFrame = [](const float delta) { (void)delta; };
+    playingState.processEvent = [](const SDL_Event& event) { (void)event; };
+
+    // --- Pause
+    pauseState.enter        = []() {};
+    pauseState.exit         = []() {};
+    pauseState.processFrame = [](const float delta) { (void)delta; };
+    pauseState.processEvent = [](const SDL_Event& event) { (void)event; };
+
+    // --- Game Over
+    gameOverState.enter        = []() {};
+    gameOverState.exit         = []() {};
+    gameOverState.processFrame = [](const float delta) { (void)delta; };
+    gameOverState.processEvent = [](const SDL_Event& event) { (void)event; };
+
+    // --- Shut Down
+    shutdownState.enter        = []() {};
+    shutdownState.exit         = []() {};
+    shutdownState.processFrame = [](const float delta) { (void)delta; };
+    shutdownState.processEvent = [](const SDL_Event& event) { (void)event; };
+
+    // ---------------------------------
+    // State Member Assertions (debug)
+    // ---------------------------------
+    // Just check to make sure state->processFrame and state->processEvent
+    // are both defined, these are just pseudo-assertions to make sure I
+    // didn't forget to define anything, they should be removed in release
+    // builds.
+#ifndef NDEBUG
+    State* debugStateAssertionChecklist[]{
+        &startState, &resetState,     &fieldSetupState, &playingState,
+        &pauseState, &countdownState, &shutdownState,   &gameOverState};
+
+    bool error = false;
+
+    for (State* state : debugStateAssertionChecklist) {
+        if ((error |= !state->processFrame)) {
+            spdlog::error("{} state does not define a frame processor!", state->tag);
+        }
+
+        if ((error |= !state->processEvent)) {
+            spdlog::error("{} state does not define an event processor!", state->tag);
+        }
+    }
+
+    if (error) {
+        abort();
+    }
+
+#endif
+}
 Game::~Game() {}
 
 // -----------------------------------------------------------------------------
-// Frame / Event Processors
+// Frame / Event Processing Dispatch
 // -----------------------------------------------------------------------------
 
-// FIXME: Not implemented.
-void Game::processFrame(float delta) {
-    (void)delta;
-
-    spdlog::warn("GAME LOOP NOT IMPLEMENTED");
-
+inline void Game::processFrame(const float delta) {
+    currentState->processFrame(delta);
     stop();
 }
+inline void Game::processEvent(const SDL_Event& event) {
+    currentState->processEvent(event);
+}
 
-// FIXME: Not implemented
-void Game::processEvent(SDL_Event& event) { (void)event; }
+// -----------------------------------------------------------------------------
+// Trigger-to-State Dispatch
+// -----------------------------------------------------------------------------
+
+void Game::transition(State* next) {
+    // If `next` is defined (a transition exists for the proved event)
+    // - see specific event callers for example.
+    if (next) {
+        // Call the exit handler of the old state, if the handler exists
+        if (currentState->exit) {
+            currentState->exit();
+        }
+        // Transition to the next state provided by the caller
+        currentState = next;
+        // Call the enter handler of the new state, if the handler exists
+        if (currentState->enter) {
+            currentState->enter();
+        }
+    }
+}
--- src/game/game.h
+++ src/game/game.h
@@ -1,13 +1,68 @@
+#include <string>
+
 #include "core/app.h"
 
+/**
+ * A fancy FSM to dispatch `App` control to `Game::State`s.
+ */
 class Game : public App {
   public:
+    struct State {
+        State(const std::string tag) : tag{tag} {};
+        const std::string tag;
+
+        std::function<void(const float)> processFrame;
+        std::function<void(const SDL_Event&)> processEvent;
+
+        std::function<void()> enter;
+        std::function<void()> exit;
+
+        // --- Event-Transition Mappings
+        // (These pointers do not "own" their targets)
+        State* onDone{nullptr};
+        State* onQuit{nullptr};
+        State* onPause{nullptr};
+        State* onNext{nullptr};
+        State* onConfirm{nullptr};
+        State* onCancel{nullptr};
+        State* onGameOver{nullptr};
+    };
+
+    friend State;
+
     Game();
     ~Game() override;
 
-    Game(Game& game)  = delete;
-    Game(Game&& game) = delete;
+    Game(Game& game)              = delete;
+    Game(Game&& game)             = delete;
+    Game& operator=(const Game&)  = delete;
+    Game& operator=(const Game&&) = delete;
 
-    void processFrame(float delta) override;
-    void processEvent(SDL_Event& event) override;
+    void processFrame(const float delta) override;
+    void processEvent(const SDL_Event& event) override;
+
+  private:
+    State* currentState;
+
+    // States;
+    State startState{"Start"};
+    State resetState{"Reset"};
+    State countdownState{"Countdown"};
+    State fieldSetupState{"Field Setup"};
+    State playingState{"Playing"};
+    State pauseState{"Pause"};
+    State gameOverState{"Game Over"};
+    State shutdownState{"Shutdown"};
+
+    // --- Triggers
+    // "Raw" transition handler
+    void transition(State* next);
+    /// Specific transition dispatch
+    std::function<void()> done{[this]() { transition(currentState->onDone); }};
+    std::function<void()> quit{[this]() { transition(currentState->onQuit); }};
+    std::function<void()> pause{[this]() { transition(currentState->onPause); }};
+    std::function<void()> next{[this]() { transition(currentState->onNext); }};
+    std::function<void()> confirm{[this]() { transition(currentState->onConfirm); }};
+    std::function<void()> cancel{[this]() { transition(currentState->onCancel); }};
+    std::function<void()> gameOver{[this]() { transition(currentState->onGameOver); }};
 };
